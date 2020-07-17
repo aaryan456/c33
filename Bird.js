8639c036f198c0cec2d9ec5e556a30d106ef36fc
aaryan456 <62750261+aaryan456@users.noreply.github.com>
2020-07-17 16:11:08 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,24 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,70,70);
+    this.image = loadImage("sprites/gh.png");
+    this.smokeImage = loadImage("sprites/smoke.png");
+    this.trajectory =[];
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+
+    super.display();
+
+    if(this.body.velocity.x > 10 && this.body.position.x > 200){
+      var position = [this.body.position.x, this.body.position.y];
+      this.trajectory.push(position);
+    }
+   
+
+    
+   
+  }
+}
